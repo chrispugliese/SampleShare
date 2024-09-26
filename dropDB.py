8728f70f48b48085f2238235ba42add4d609faa0
CrusaderCoder73 <mbustam9@msudenver.edu>
2024-09-26 12:09:08 -0600

    Created Files to Add and Drop DB and Working on DM
    Created Files to add and drop the database and now working on data model

---
+++ dropDB.py
@@ -0,0 +1,18 @@
+# In case for some reason you need to drop the database this code will delete the database
+# Run this code by typing the command "python dropDB.py"
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    # your username maybe different so enter the user name that you entered when you configured mysql
+    user = 'root',
+    # you password maybe different so enter the password you setup on mysql
+    password = 'YourMom!73'
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("DROP DATABASE sampleshare")
+
+print("Database Successfully Dropped !!")
--- website/models.py
+++ website/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+# ------User-Profiles---------------
+# username = VARCHAR(50) NOT NULL
+# dataOfBirth = VARCHAR(50) NOT NULL
+# email = VARCHAR(50) NOT NULL
+# password = VARCHAR(50) NOT NULL
+# userPhote = VARCHAR (50) NOT NULL
+# bio = TEXT NOT NULL
+#------------------------------------
+class UserProfile(models.Model):
+    username = models.CharField(max_length=50)
+    dateOfBirth = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    userPhoto = models.CharField(max_length=50)
+    bio = models.TextField()
+    numberOfFollowers = models.IntegerField()
+
+# ------Samples------
+# samplename = VARCHAR(50) NOT NULL
+# fileLocation = VARCHAR(50) NOT NULL
+# isPublic = BOOLEAN NOT NULL
+#------------------------------------
+class Sample(models.Model):
+    sampleName = models.CharField(max_length = 50)
+    fileLocation = models.CharField(max_length= 100)
+    isPublic = models.BooleanField()
+    # Many to Many with UserProfiles
+    userProfiles = models.ManyToManyField(UserProfile)
+
+class Chat(models.Model):
+    chatName = models.CharField(max_length = 50)
+    chatTimeStamp = models.DateTimeField(auto_new_add =True)
+
+class Message(models.Model):
+    message = models.TextField()
+    messageTimeStamp = models.TextField()
