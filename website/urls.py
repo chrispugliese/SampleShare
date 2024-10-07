6c7e531d7e4def2f2221920b14dee86b1d18a5d6
leviathan <gonzalezsaul31@gmail.com>
2024-10-06 20:31:42 -0600

    i ended up not liking the playsound project, i think it will bring in a lot of issues since it will cause the page to be unresponsive until the audio ends. i will look into the html player and maybe use js to make it look cool.

--- website/urls.py
+++ website/urls.py
@@ -3,11 +3,14 @@ from . import views
 from django.http import Http404
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('user/<int:user_id>/', views.user_detail, name='user_detail'),  # Dynamic user page
-    path('user/', views.page_not_found),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
-
+    path("", views.home, name="home"),
+    path(
+        "user/<int:user_id>/", views.user_detail, name="user_detail"
+    ),  # Dynamic user page
+    path("user/", views.page_not_found),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
+    path("sample/<int:sample_id>/", views.sample_player, name="sample_player"),
 ]
+
--- website/views.py
+++ website/views.py
@@ -1,56 +1,89 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
-from .models import UserProfile
+from django.http import Http404, HttpResponse
+from .models import UserProfile, Sample
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from playsound import playsound, PlaysoundException  # dont want this
+import os
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    return render(request, "home.html", {})
+
 
 def user_detail(request, user_id):
-    user_profile = get_object_or_404(UserProfile, id=user_id) #Query UserProfile by ID
-    return render(request, 'user_detail.html', {'user_profile': user_profile})
+    user_profile = get_object_or_404(UserProfile, id=user_id)  # Query UserProfile by ID
+    return render(request, "user_detail.html", {"user_profile": user_profile})
+
 
 def page_not_found(request):
     raise Http404("Page not here tho")
 
+
 def login_user(request):
-    if request.method == 'POST':
-            username = request.POST['username']
-            password = request.POST['password']
-            #Authenticate
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                messages.success(request, "Welcome back")
-                return redirect('home')
-            else: 
-                messages.success(request, "Login information incorrect")
-                return redirect('login')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Welcome back")
+            return redirect("home")
+        else:
+            messages.success(request, "Login information incorrect")
+            return redirect("login")
     else:
-        return render(request, 'login.html')
+        return render(request, "login.html")
+
 
 def logout_user(request):
     logout(request)
     messages.success(request, "Logged out")
-    return redirect('home')
+    return redirect("home")
+
 
 def register_user(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            #Authenticate & log in
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            date_of_birth = form.cleaned_data['date_of_birth']
+            # Authenticate & log in
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            date_of_birth = form.cleaned_data["date_of_birth"]
             user = authenticate(username=username, password=password)
             login(request, user)
             messages.success(request, "Account Registered!")
-            return redirect('home')
+            return redirect("home")
     else:
         form = SignUpForm()
-        return render(request, 'register.html', {'form':form})
-    return render(request, 'register.html', {'form':form})
+        return render(request, "register.html", {"form": form})
+    return render(request, "register.html", {"form": form})
+
+
+# i dont like the playsound package, i will refactor this to use the html audio widget, maybe use js to make it cool.
+def sample_player(request, sample_id):
+    sample = get_object_or_404(Sample, id=sample_id)
+    file_path_to_sample = sample.fileLocation
+    try:
+        if not os.path.exists(file_path_to_sample):
+            raise FileNotFoundError(
+                f"The sample file at {file_path_to_sample} does not exist."
+            )
+
+        playsound(file_path_to_sample)
+        message = f"Playing {sample.sampleName}"
+
+    except FileNotFoundError as fnf_error:
+        message = f"File error: {fnf_error}"
+
+    except PlaysoundException as ps_error:
+        message = f"Playsound error: {ps_error}"
+
+    except Exception as e:
+        message = f"An unexpected error has occurred: {e}"
+
+    return render(request, "sample_player.html", {"sample": sample, "message": message})
