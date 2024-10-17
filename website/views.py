0055e3ca056614b016d1124b0c93e05389f95eca
chrispugliese <89790420+chrispugliese@users.noreply.github.com>
2024-10-17 15:21:33 -0600

    User can now delete their account and it asks for confirmation

--- website/views.py
+++ website/views.py
@@ -272,10 +272,11 @@ def delete_comment(request, pk):
 # ---------------------------------Delete Account-----------------------------------#
 
 def delete_account(request):
-    if request.user.is_authenticated:
+    if request.method == "POST":
         request.user.delete()
         messages.success(request, "Your account has been deleted. ")
         return redirect("home")
+    return render(request, "confirm_delete_account.html")
 
 # --------------------------------------------------------------------#
 
