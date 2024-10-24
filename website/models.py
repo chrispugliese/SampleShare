0caf4aa7dc137d9ca17f4faf3d274f745eaaa79a
DylanCReed <dylancreed96@gmail.com>
2024-10-24 15:33:10 -0700

    Update models.py

--- website/models.py
+++ website/models.py
@@ -27,6 +27,13 @@ class UserProfile(models.Model):
     def __str__(self):
         return str(self.user)
 
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(UserProfile, related_name='sent_requests', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(UserProfile, related_name='received_requests', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.from_user} -> {self.to_user}"
+
 
 # ------Samples------
 # samplename = VARCHAR(50) NOT NULL
