86ee5dfe4889422820a236370118918fa06aa7c1
leviathan <gonzalezsaul31@gmail.com>
2024-10-28 02:08:18 -0600

    nothing so far, lost all motivation to care

--- static/js/sample_player.js
+++ static/js/sample_player.js
@@ -17,8 +17,21 @@ const playerControls = {
 
 let sound = null;
 let currentUrl = ''
-
+let wavesurfer = null
 const playPause = (url) => {
+	if (!wavesurfer) {
+		const wavesurfer = WaveSurfer.create({
+			container: '#waveform',
+			waveColor: '#4F4A85',
+			progressColor: '#383351',
+			url: [url],
+		});
+
+		wavesurfer.on('click', () => {
+			wavesurfer.play()
+		});
+	}
+
 	if (url !== currentUrl) {
 		if (sound) {
 			sound.stop();
