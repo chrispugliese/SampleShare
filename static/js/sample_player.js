542e3140c29403ef4cab3f34585550c9bcc20d72
leviathan <gonzalezsaul31@gmail.com>
2024-10-28 17:23:11 -0600

    got waveforms, had to change the js library etc, sorry mat

--- static/js/sample_player.js
+++ static/js/sample_player.js
@@ -9,41 +9,64 @@
 // 	sound.play();
 // }
 
-const playerControls = {
-	playBtn: document.getElementById('playBtn'),
-	nxtBtn: document.getElementById('nxtBtn'),
-	prvBtn: document.getElementById('prvBtn'),
+// const playerControls = {
+// 	playBtn: document.getElementById('playBtn'),
+// 	nxtBtn: document.getElementById('nxtBtn'),
+// 	prvBtn: document.getElementById('prvBtn'),
+// };
+// 
+// let currentUrl = ''
+// let wavesurfer = null
+// 
+// const initWaveSurfer = (url) => {
+// 	wavesurfer = WaveSurfer.create({
+// 		container: '#waveform',
+// 		waveColor: '#4F4A85',
+// 		progressColor: '#383351',
+// 		backend: 'MediaElement',
+// 	});
+// 	wavesurfer.load(url);
+// };
+// 
+// const playPause = (url) => {
+// 	if (!wavesurfer) {
+// 		initWaveSurfer(url)
+// 	}
+// 
+// 	if (url !== currentUrl) {
+// 		wavesurfer.load(url);
+// 		currentUrl = url
+// 	}
+// 
+// 	wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
+// };
+// 
+// playerControls.playBtn.addEventListener('click', () => playPause(currentUrl));
+//
+
+document.addEventListener('DOMContentLoaded', () => {
+	// Get the first sample URL from the hidden input
+	const sampleUrl = document.querySelector('.sample-url')?.value;
+
+	if (sampleUrl) {
+		initWaveSurfer(sampleUrl);
+		// Automatically play the audio when loaded
+	}
+});
+
+let wavesurfer = null;
+let currentUrl = '';
+
+const initWaveSurfer = (url) => {
+	wavesurfer = WaveSurfer.create({
+		container: '{waveform-#}',  // Assuming we want the first waveform to play automatically
+		waveColor: '#4F4A85',
+		progressColor: '#383351',
+		backend: 'MediaElement',
+	});
+	wavesurfer.load(url);
+	wavesurfer.on('interaction', () => {
+		wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
+	});
 };
 
-let sound = null;
-let currentUrl = ''
-let wavesurfer = null
-const playPause = (url) => {
-	if (!wavesurfer) {
-		const wavesurfer = WaveSurfer.create({
-			container: '#waveform',
-			waveColor: '#4F4A85',
-			progressColor: '#383351',
-			url: [url],
-		});
-
-		wavesurfer.on('click', () => {
-			wavesurfer.play()
-		});
-	}
-
-	if (url !== currentUrl) {
-		if (sound) {
-			sound.stop();
-		}
-		sound = new Howl({
-			src: [url],
-			autoplay: false,
-			loop: false,
-			html5: true,
-			format: ['mp3', 'wav'],
-		});
-		currentUrl = url;
-	}
-	sound.playing() ? sound.pause() : sound.play();
-};
